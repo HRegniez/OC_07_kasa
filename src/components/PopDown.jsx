a7eb7359aae5319e94089dfbdd41dfd0a0929e02
HRegniez <henriregniez124@gmail.com>
2023-08-31 17:46:13 +0200

    UPdate

--- src/components/PopDown.jsx
+++ src/components/PopDown.jsx
@@ -1,5 +1,6 @@
 import { useState} from 'react'
 import arrow from '../assets/arrow.svg'
+import '../Style/components/popDown.css'
 
 function PopDown(props) {
     const {title, contents} = props
@@ -14,14 +15,15 @@ function PopDown(props) {
 
 
   return (
-    <article className={`popdown ${toggle ? "active" : ""}`}>
+    <article className={`popdown ${toggle ? "open" : ""}`}>
         <div className='popdown_title'><h2>{title}</h2><img src={arrow} onClick={click} alt="bouton pour plus de détails" /></div>
-        {contents.map(content => 
-            <div key={content} className="popdown_detail">
-                {content}
-            </div>                    
-        )
-        }
+        <div className="popdown_detail">
+            {contents.map(content => 
+                (<div>{content}</div>)
+            )
+            }    
+        </div>
+        
     </article> 
   )
 }
