ab7e935125cffa0b5193fa3070be5439607c002c
HRegniez <henriregniez124@gmail.com>
2023-09-12 12:59:58 +0200

    Slider counter

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -5,7 +5,7 @@ import arrowRight from '../assets/arrow_right.svg'
 
 function Carousel(props) {
   const pictures = props.pictures
-  console.log(pictures)
+  console.log(pictures.length)
   const [indexCurrent, setIndexCurrent] = useState(0)
 
   const forward = () => (
@@ -15,12 +15,17 @@ function Carousel(props) {
     setIndexCurrent((indexCurrent - 1 + pictures.length) % pictures.length)
   )
 
+    const counter = () => (
+      <span className='carousel_counter'>{`${indexCurrent + 1}/${pictures.length}`}</span>
+    )
+
   return (
     <div className='carousel'>
       <img className='carousel_img' src={pictures[indexCurrent]} alt="" />
       
       <img src={arrowLeft} className={pictures.length <= 1 ? "hidden" : "carousel_arrow--left"} onClick={backward}/>
       <img src={arrowRight} className={pictures.length <= 1 ? "hidden" : "carousel_arrow--right"}  onClick={forward}/>
+      {pictures.length > 1 ? counter() : "" }
     </div>
   )
 }
